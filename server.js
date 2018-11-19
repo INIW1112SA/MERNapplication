75e17f28405fba6ce3c1477d765daf284b68142d
as351910 <AS351910@wipro.com>
2018-11-19 17:50:15 +0530

    user model

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -8,6 +8,11 @@ const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //DB config
 const database = require('./config/keys').mongoURL;
@@ -22,9 +27,9 @@ mongoose
 app.get('/', (req,res) => res.send("Hello fullstack"));
 
 //use routes
-app.use('/users',users);
-app.use('/profile',profile);
-app.use('/posts',posts);
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
 const port = process.env.port || 5000;
 
 app.listen(port, ()=> console.log(`server is listening to ${port}`));
